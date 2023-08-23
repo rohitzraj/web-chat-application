478ea26a479f8823d072df0ebbf9a7d6fbade7ae
rohitzraj <106408680+rohitzraj@users.noreply.github.com>
2023-08-23 21:37:41 +0530

    change in auth

--- chatApp-main/middleware/auth.js
+++ chatApp-main/middleware/auth.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const requireauth = (req, res, next) => {
     const token = req.cookies.login;
     if (token) {
-        jwt.verify(token, "ankitgarg", (err, decodedToken) => {
+        jwt.verify(token, "rohitraj", (err, decodedToken) => {
             if (err) {
                 console.log(err.message);
                 res.redirect("/login");
